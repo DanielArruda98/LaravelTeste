53139c5bd116da486aa08618a07b7e85eda8d695
DanielArruda98 <arruda.daniel20@gmail.com>
2021-02-02 21:17:07 -0300

    version start

---
+++ example-app/resources/views/pagina1.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <p>Página 1</p>
+@endsection
---
+++ example-app/resources/views/pagina3.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <p>Página 3</p>
+@endsection
